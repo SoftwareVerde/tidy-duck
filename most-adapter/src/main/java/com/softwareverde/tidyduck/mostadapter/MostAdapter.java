13f31432408679704cdb076660a0f93c09d2e09a
Andrew Groot <groot@softwareverde.com>
2017-06-05 17:34:17 -0400

    Cleanup on MostAdapter.

--- most-adapter/src/main/java/com/softwareverde/tidyduck/mostadapter/MostAdapter.java
+++ most-adapter/src/main/java/com/softwareverde/tidyduck/mostadapter/MostAdapter.java
@@ -1,18 +1,13 @@
 package com.softwareverde.tidyduck.mostadapter;
 
 import com.softwareverde.tidyduck.Version;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class MostAdapter {
-
-    private final Logger _logger = LoggerFactory.getLogger(getClass());
 
     public String getMostXml(Version version) throws MostAdapterException {
         try {
