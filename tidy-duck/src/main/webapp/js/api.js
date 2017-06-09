c0c2c4199c217066658fcb13616ac544b2f0bfca
Andrew Groot <groot@softwareverde.com>
2017-06-09 15:40:14 -0400

    Added api.js to handle API calls using fetch.

---
+++ tidy-duck/src/main/webapp/js/api.js
@@ -0,0 +1,42 @@
+
+var API_ENDPOINT_PREFIX = '/tidy-duck/';
+
+function jsonFetch(request) {
+    return fetch(request)
+        .then(function (response) {
+            return response.json();
+        })
+}
+
+// calls callbackFunction with an array of function catalogs
+function getFunctionCatalogsForVersionId(versionId, callbackFunction) {
+    var endpoint = API_ENDPOINT_PREFIX + 'api/v1/function-catalog?version_id=' + versionId;
+    jsonFetch(endpoint)
+        .then(function (data) {
+            if (data.wasSuccess) {
+                callbackFunction(data.functionCatalogs);
+            } else {
+                console.log('Unable to get function catalogs for version ' + versionId + ': ' + data.error);
+            }
+        });
+}
+
+// calls callbackFunction with new function catalog ID
+function insertFunctionCatalog(versionId, functionCatalog, callbackFunction) {
+    var request = new Request(API_ENDPOINT_PREFIX + '/api/v1/function-catalog', {
+        method: 'POST',
+        body: functionCatalog
+    })
+    jsonFetch(request)
+        .then(function (data) {
+            if (data.wasSuccess) {
+                callbackFunction(data.functionCatalogId);
+            } else {
+                console.log('Unable to insert function catalog for version ' + versionId + ': ' + data.error);
+            }
+        });
+}
+
+function exportFunctionCatalogToMost(functionCatalogId) {
+    window.open('/v1/generate-most?function_catalog_id=' + function_catalog_id);
+}
