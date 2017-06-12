36c4dfdbe9529f2410ebb230351f960bd427dab7
Josh Green <josh@softwareverde.com>
2017-06-12 14:56:48 -0400

    Updated OSX Deploy-Local to use full path.

--- app/scripts/osx/deploy-local.sh
+++ app/scripts/osx/deploy-local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DESTINATION='../../tomcat'
+# DESTINATION='../../tomcat'
+DESTINATION='/usr/local/Cellar/tomcat/8.5.15/libexec/webapps'
 
 rm ${DESTINATION}/*.war 2>/dev/null
 ./gradlew war && cp $(ls -tr build/libs/*.war | tail -1) ${DESTINATION}/tidy-duck.war && chmod 770 ${DESTINATION}/*.war
