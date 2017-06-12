f851299ad3621e741d338b84780776fc2e5ded28
Josh Green <josh@softwareverde.com>
2017-06-12 16:28:08 -0400

    Added Test for Function Catalog DB loading and inflation.

---
+++ app/src/test/java/com/softwareverde/tidyduck/database/TestDataLoader.java
@@ -0,0 +1,27 @@
+package com.softwareverde.tidyduck.database;
+
+
+import com.softwareverde.database.DatabaseConnection;
+import com.softwareverde.database.DatabaseException;
+import com.softwareverde.database.Query;
+import com.softwareverde.util.IoUtil;
+
+import java.sql.Connection;
+
+public class TestDataLoader {
+    public static void initDatabase(final DatabaseConnection<Connection> databaseConnection) throws DatabaseException {
+        databaseConnection.executeSql(new Query(IoUtil.getResource("/sql/init.sql")));
+    }
+
+    public static void insertFakeCompany(final DatabaseConnection<Connection> databaseConnection) throws DatabaseException {
+        databaseConnection.executeSql(new Query("INSERT INTO companies (name) VALUES ('Software Verde, LLC')"));
+    }
+
+    public static void insertFakeAccount(final DatabaseConnection<Connection> databaseConnection) throws DatabaseException {
+        databaseConnection.executeSql(new Query("INSERT INTO accounts (name, company_id) VALUES ('Josh Green', 1)"));
+    }
+
+    public static void insertFakeVersion(final DatabaseConnection<Connection> databaseConnection) throws DatabaseException {
+        databaseConnection.executeSql(new Query("INSERT INTO versions (name, owner_id) VALUES ('Version 1', 1)"));
+    }
+}
