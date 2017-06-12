927fb2bc3ea73866c5307cf40118ae723f0cd970
Andrew Groot <groot@softwareverde.com>
2017-06-12 10:35:56 -0400

    Fixes for foreign keys.

--- sql/init.sql
+++ sql/init.sql
@@ -12,41 +12,45 @@ DROP TABLE IF EXISTS companies;
 CREATE TABLE companies (
     id int unsigned NOT NULL PRIMARY KEY auto_increment,
     name varchar(255) NOT NULL
-);
-INSERT INTO companies (name) VALUES ('Software Verde, LLC');
-INSERT INTO companies (name) VALUES ('Honda R+D');
+) ENGINE=INNODB CHARACTER SET UTF8;
 
 CREATE TABLE authors (
     id int unsigned NOT NULL PRIMARY KEY auto_increment,
     name varchar(255) NOT NULL,
-    company_id integer NOT NULL REFERENCES companies (id)
-);
+    company_id int unsigned NOT NULL,
+    FOREIGN KEY (company_id) REFERENCES companies (id)
+) ENGINE=INNODB CHARACTER SET UTF8;
 
 CREATE TABLE function_catalogs (
     id int unsigned NOT NULL PRIMARY KEY auto_increment,
     name varchar(255) NOT NULL,
     release_version varchar(255) NOT NULL,
     release_date date NOT NULL,
-    author_id int unsigned NOT NULL REFERENCES authors (id),
-    company_id int unsigned NOT NULL REFERENCES companies (id)
-);
+    author_id int unsigned NOT NULL,
+    company_id int unsigned NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (company_id) REFERENCES companies (id)
+) ENGINE=INNODB CHARACTER SET UTF8;
 
 CREATE TABLE accounts (
     id int unsigned NOT NULL PRIMARY KEY auto_increment,
     name varchar(255) NOT NULL
-);
+) ENGINE=INNODB CHARACTER SET UTF8;
 
 CREATE TABLE versions (
     id int unsigned NOT NULL PRIMARY KEY auto_increment,
     name varchar(255) NOT NULL,
     is_committed boolean NOT NULL DEFAULT FALSE,
-    owner_id int unsigned NOT NULL REFERENCES accounts (id)
-);
+    owner_id int unsigned NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES accounts (id)
+) ENGINE=INNODB CHARACTER SET UTF8;
 
 CREATE TABLE versions_function_catalogs (
     id int unsigned NOT NULL PRIMARY KEY auto_increment,
-    version_id int unsigned NOT NULL REFERENCES versions (id),
-    function_catalog_id int unsigned NOT NULL REFERENCES function_catalogs (id),
-    is_committed boolean NOT NULL DEFAULT FALSE
-);
+    version_id int unsigned NOT NULL,
+    function_catalog_id int unsigned NOT NULL,
+    is_committed boolean NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (version_id) REFERENCES versions (id),
+    FOREIGN KEY (function_catalog_id) REFERENCES function_catalogs (id)
+) ENGINE=INNODB CHARACTER SET UTF8;
 
--- sql/load_dummy_data.sql
+++ sql/load_dummy_data.sql
@@ -3,14 +3,8 @@ INSERT INTO companies (name) VALUES ('Software Verde, LLC');
 
 INSERT INTO authors (name, company_id) VALUES ('Josh Green', 1);
 
-INSERT INTO accounts (name, company_id) VALUES ('Josh Green', 1);
-
-INSERT INTO function_catalogs (name, release_version, release_date, author_id, company_id)
-VALUES ('Test 1', '1.0.0', NOW(), 1, 1);
+INSERT INTO accounts (name) VALUES ('Josh Green');
 
 INSERT INTO versions (name, owner_id)
 VALUES ('Version 1', 1);
-
-INSERT INTO versions_function_catalogs (version_id, function_catalog_id)
-VALUES (1, 1);
 
