1d1388fd85fb1f62944b0eadbaf8de5971cc035a
Eliot Lesar <lesar@softwareverde.com>
2017-06-13 12:05:24 -0400

    Committing minor changes to app.js before pulling.

--- app/src/main/webapp/js/react/app.js
+++ app/src/main/webapp/js/react/app.js
@@ -49,6 +49,7 @@ class App extends React.Component {
 
     onFunctionCatalogSave(functionCatalog) {
         // TODO: Call FunctionCatalog api update function.
+
     }
 
     onRootNavigationItemClicked() {
