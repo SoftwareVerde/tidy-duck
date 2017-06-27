82c01d2c88e7e9baed5a17a14f22b28dc94a75b0
Andrew Groot <groot@softwareverde.com>
2017-06-27 14:32:26 -0400

    Fix to allow OSX deploy-local script to be run from anywhere.

--- app/scripts/osx/deploy-local.sh
+++ app/scripts/osx/deploy-local.sh
@@ -4,5 +4,7 @@
 DESTINATION='/usr/local/Cellar/tomcat/8.5.15/libexec/webapps'
 
 rm ${DESTINATION}/ROOT* 2>/dev/null
+cd "$(dirname $0)/../../"
 ./gradlew war && cp $(ls -tr build/libs/*.war | tail -1) ${DESTINATION}/ROOT.war && chmod 770 ${DESTINATION}/ROOT.war
+cd -
 
