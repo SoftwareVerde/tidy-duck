41b283aa99679612c20b8eeca9d8ec2d90bb4400
Josh Green <josh@softwareverde.com>
2017-07-10 14:37:48 -0400

    Deleted InvalidFunctionCatalogIdException.

--- app/src/main/java/com/softwareverde/tidyduck/database/InvalidFunctionCatalogIdException.java
+++
@@ -1,7 +0,0 @@
-package com.softwareverde.tidyduck.database;
-
-public class InvalidFunctionCatalogIdException extends RuntimeException {
-    InvalidFunctionCatalogIdException(final Object catalogId, final Exception exception) {
-        super("Invalid Function Catalog ID: "+ catalogId, exception);
-    }
-}
