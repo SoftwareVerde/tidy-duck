970422eab87ec2516ce6d02b7cff7b91fbdcf8f7
Eliot Lesar <lesar@softwareverde.com>
2017-07-18 17:08:18 -0400

    Added helper functions for updating a most function in the database.

--- app/src/main/java/com/softwareverde/tidyduck/database/MostFunctionDatabaseManager.java
+++ app/src/main/java/com/softwareverde/tidyduck/database/MostFunctionDatabaseManager.java
@@ -21,6 +21,50 @@ class MostFunctionDatabaseManager {
         _associateMostFunctionWithMostInterface(mostInterfaceId, mostFunction.getId());
     }
 
+    // TODO: update committed MostFunction method.
+    private void _updateUncommittedMostFunction(MostFunction proposedMostFunction) throws DatabaseException {
+        final String name = proposedMostFunction.getName();
+        final String mostId = proposedMostFunction.getMostId();
+        final long functionStereotypeId = proposedMostFunction.getFunctionStereotype().getId();
+        final String description = proposedMostFunction.getDescription();
+        final String release = proposedMostFunction.getRelease();
+        final long authorId = proposedMostFunction.getAuthor().getId();
+        final long companyId = proposedMostFunction.getCompany().getId();
+        final long returnTypeId = proposedMostFunction.getReturnType().getId();
+        final long mostFunctionId = proposedMostFunction.getId();
+
+        final Query query = new Query("UPDATE functions SET (name, most_id, category, function_stereotype_id, description, release_version, account_id, company_id, return_type_id, supports_notification) WHERE id = ? VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+                .setParameter(name)
+                .setParameter(mostId)
+                .setParameter(mostFunction.getFunctionType())
+                .setParameter(functionStereotypeId)
+                .setParameter(description)
+                .setParameter(release)
+                .setParameter(authorId)
+                .setParameter(companyId)
+                .setParameter(returnTypeId)
+                .setParameter(supportsNotification ? 1 : 0)
+                .setParameter(mostFunctionId)
+        ;
+
+        _databaseConnection.executeSql(query);
+
+        // Clear out operations and parameters, then add current ones if necessary.
+        _removeInputParametersFromFunction(mostFunctionId);
+        _removeOperationsFromFunction(mostFunctionId);
+
+        if ("Method".equals(mostFunction.getFunctionType())) {
+            Method method = (Method) mostFunction;
+            for (final MostFunctionParameter parameter : method.getInputParameters()) {
+                _addInputParameterToFunction(mostFunctionId, parameter);
+            }
+        }
+
+        for (final Operation operation : mostFunction.getOperations()) {
+            _addOperationToFunction(mostFunctionId, operation);
+        }
+    }
+
     private void _insertMostFunction(MostFunction mostFunction) throws DatabaseException {
         final String name = mostFunction.getName();
         final String mostId = mostFunction.getMostId();
@@ -75,11 +119,27 @@ class MostFunctionDatabaseManager {
         _databaseConnection.executeSql(query);
     }
 
+    private void _removeInputParametersFromFunction(final long mostFunctionId) throws DatabaseException {
+        final Query query = new Query("DELETE * FROM function_parameters WHERE function_id = ?")
+                .setParameter(mostFunctionId)
+                ;
+
+        _databaseConnection.executeSql(query);
+    }
+
     private void _addOperationToFunction(final long newFunctionId, final Operation operation) throws DatabaseException {
         final Query query = new Query("INSERT INTO functions_operations (function_id, operation_id) VALUES (?, ?)")
             .setParameter(newFunctionId)
             .setParameter(operation.getId())
         ;
+
+        _databaseConnection.executeSql(query);
+    }
+
+    private void _removeOperationsFromFunction(final long mostFunctionId) throws DatabaseException {
+        final Query query = new Query("DELETE * FROM functions_operations WHERE function_id = ?")
+                .setParameter(mostFunctionId)
+                ;
 
         _databaseConnection.executeSql(query);
     }
