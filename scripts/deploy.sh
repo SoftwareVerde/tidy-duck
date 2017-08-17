11d4a7a2e1f6c06263f607a921f8b55c1bcc314f
Josh Green <josh@softwareverde.com>
2017-08-17 12:02:38 -0400

    Updated INSTALL script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ DEPLOY_PATH="/var/lib/tomcat7/webapps/"
 TARGET_WAR_FILE_NAME="ROOT.war"
 
 FILE=$1
-SERVER=$1
+SERVER=$2
 if [[ ! -f ${FILE} ]]; then
     echo -e "Unable to find warfile: ${FILE}\nUsage: $0 <path-to-warfile> <url-to-server>"
     exit 1
@@ -17,8 +17,8 @@ if [[ -z ${SERVER} ]]; then
     exit 1
 fi
 
-scp "${FILE}" ${SERVER}:~/${TARGET_WAR_FILE_NAME}
-ssh -t ${SERVER} "echo Server-Side Sudo Password:; sudo rm -rf ${DEPLOY_PATH}/*; sudo mv ${TARGET_WAR_FILE_NAME} ${DEPLOY_PATH}; sudo chown tomcat7:tomcat7 ${DEPLOY_PATH}/${TARGET_WAR_FILE_NAME}"
+scp "${FILE}" "${SERVER}":"~/${TARGET_WAR_FILE_NAME}"
+ssh -t "${SERVER}" "echo Server-Side Sudo Password:; sudo rm -rf ${DEPLOY_PATH}/*; sudo mv ${TARGET_WAR_FILE_NAME} ${DEPLOY_PATH}; sudo chown tomcat7:tomcat7 ${DEPLOY_PATH}/${TARGET_WAR_FILE_NAME}"
 
 echo "Done."
 
--- scripts/make.sh
+++ scripts/make.sh
@@ -16,14 +16,6 @@ rm -rf .gradle/
 cd -
 
 file=$(ls -tr ${WAR_MODULE_PATH})
-echo ${file}
-
-if [[ $? == 0 ]]; then
-    if [[ ! -f $file ]]; then
-        echo "$WAR_MODULE_PATH does not exist."
-        exit 1
-    fi
-    scp $file $SERVER:~/$TARGET_WAR_FILE_NAME
-    ssh -t $SERVER "sudo rm -rf $DEPLOY_PATH/*; sudo mv $TARGET_WAR_FILE_NAME $DEPLOY_PATH; sudo chown tomcat7:tomcat7 $DEPLOY_PATH/$TARGET_WAR_FILE_NAME"
-fi
+echo "$(cd "$(dirname "${file}")"; pwd)/$(basename "${file}")"
+echo
 
