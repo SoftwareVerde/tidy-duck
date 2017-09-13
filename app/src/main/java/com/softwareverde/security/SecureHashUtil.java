497b46a950e39a6551c31bdb7852a4ec66accf16
Eliot Lesar <lesar@softwareverde.com>
2017-09-13 11:59:50 -0400

    Added Security pacakage with SecureHashUtil class. Next step is to unit test it.

---
+++ app/src/main/java/com/softwareverde/security/SecureHashUtil.java
@@ -0,0 +1,72 @@
+package com.softwareverde.security;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+public class SecureHashUtil {
+
+    public static String hashWithPbkdf2 (final String s) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        final int iterations = 4096;
+        final int randomizedIterations = (int)(iterations + Math.round(iterations * Math.random()));
+        final int keyLength = 512;
+
+        final char[] passwordChars = s.toCharArray();
+        final byte[] salt = getSalt();
+
+        final PBEKeySpec pbeKeySpec = new PBEKeySpec(passwordChars, salt, randomizedIterations, keyLength);
+        final SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        final byte [] hash = secretKeyFactory.generateSecret(pbeKeySpec).getEncoded();
+
+        return randomizedIterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+
+    public static boolean validateHashWithPbkdf2 (final String s, final String h) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        final String[] parts = h.split(":");
+        final int iterations = Integer.parseInt(parts[0]);
+        final byte[] salt = fromHex(parts[1]);
+        final byte[] hash = fromHex(parts[2]);
+
+        final PBEKeySpec pbeKeySpec = new PBEKeySpec(s.toCharArray(), salt, iterations, hash.length * 8);
+        final SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        final byte[] testHash = secretKeyFactory.generateSecret(pbeKeySpec).getEncoded();
+
+        int diff = hash.length ^ testHash.length;
+        for (int i = 0; i < hash.length && i < testHash.length; i++) {
+            diff |= hash[i] ^ testHash[i];
+        }
+
+        return diff == 0;
+     }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        final SecureRandom secureRandom = SecureRandom.getInstance("SHA1RNG");
+        final byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(final byte[] array) throws NoSuchAlgorithmException {
+        final BigInteger bigInteger = new BigInteger(1, array);
+        final String hex = bigInteger.toString(16);
+
+        final int paddingLength = (array.length * 2) - hex.length();
+        if (paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        }
+        else {
+            return hex;
+        }
+    }
+
+    private static byte[] fromHex(final String hex) throws NoSuchAlgorithmException {
+        final byte[] hexBytes = new byte[hex.length() / 2];
+        for (int i = 0; i < hexBytes.length; i++) {
+            hexBytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return hexBytes;
+    }
+}
