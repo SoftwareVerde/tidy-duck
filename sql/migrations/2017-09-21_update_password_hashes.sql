3dfc6b7c919736736175664a60e7df5f34d9e733
Andrew Groot <groot@softwareverde.com>
2017-09-21 11:52:07 -0400

    Added migration for updating password hashes.

---
+++ sql/migrations/2017-09-21_update_password_hashes.sql
@@ -0,0 +1,4 @@
+
+-- set all passwords to 'quack quack' (new hashing algorithm)
+UPDATE accounts SET password = '10814:943229eda758ee6c3006990153aff16b:e11cd806ea052d20c19c0455f9eb0218efe9ef334161f09e2616893ac675abac4d29c83eb03c3952d7509cc06269f51229d4a272ac0137773fc4e67b78d87cb7';
+
