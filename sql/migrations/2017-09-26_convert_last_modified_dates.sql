ea2e2b9f3e96cc1b2b9d8d8e0feef22557ea3593
Andrew Groot <groot@softwareverde.com>
2017-09-26 16:03:09 -0400

    Added migration for converting last modified dates to datetime types.

---
+++ sql/migrations/2017-09-26_convert_last_modified_dates.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE function_blocks CHANGE last_modified_date last_modified_date DATETIME NOT NULL;
+ALTER TABLE interfaces CHANGE last_modified_date last_modified_date DATETIME NOT NULL;
+
