2c18636e3ae62b189913bb8e9977aec15dc52d30
Andrew Groot <groot@softwareverde.com>
2018-02-13 11:54:38 -0500

    Detecting root of history for this page and skipping back to prior page.

--- app/src/main/webapp/js/app.js
+++ app/src/main/webapp/js/app.js
@@ -18,8 +18,14 @@ function redirectToLogin() {
 })();
 
 if (typeof history.pushState === "function") {
+    history.pushState("tidy-duck-root", null, null);
     window.onpopstate = function(event) {
         let state = event.state;
+        if (state == "tidy-duck-root") {
+            // return to prior page
+            history.go(-2); // skip over root state and original state
+            return;
+        }
         app.App._instance.handleRoleClick(state.roleName, state.subRoleName, false);
     };
 }
