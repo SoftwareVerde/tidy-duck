a3077412c0f8a37d90cfd44724bee608c4134202
Eliot Lesar <lesar@softwareverde.com>
2022-06-27 15:00:32 -0400

    Added new test accounts that are initially inserted into the database when the application is started.

--- sql/load-qa-data.sql
+++ sql/load-qa-data.sql
@@ -1,4 +1,5 @@
 INSERT INTO companies (name) VALUES ('Software Verde, LLC');
 INSERT INTO companies (name) VALUES ('Company Name, INC');
 
-INSERT INTO accounts (username, name, password, company_id) VALUES ('test@softwareverde.com', 'Pat Test', 'Argon2$2$19$12$16$2$F3EE5AB0A855880CDFCC44576D8F43F8$32$B534941D2FBBEA6EE6C460B7D10E5F8592BD39F23497C9ADAE6ED35BAF4BACA7', 1);
+INSERT INTO accounts (username, name, password, company_id) VALUES ('pat.test@softwareverde.com', 'Pat Test', 'Argon2$2$19$12$16$2$F3EE5AB0A855880CDFCC44576D8F43F8$32$B534941D2FBBEA6EE6C460B7D10E5F8592BD39F23497C9ADAE6ED35BAF4BACA7', 1);
+INSERT INTO accounts (username, name, password, company_id) VALUES ('taylor.test@softwareverde.com', 'Taylor Test', 'Argon2$2$19$12$16$2$F3EE5AB0A855880CDFCC44576D8F43F8$32$B534941D2FBBEA6EE6C460B7D10E5F8592BD39F23497C9ADAE6ED35BAF4BACA7', 1);
