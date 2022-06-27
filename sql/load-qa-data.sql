1a9b78fc47782fbc018850ca38b57e65c4899aa2
Eliot Lesar <lesar@softwareverde.com>
2022-06-27 14:32:07 -0400

    Removed automatic addition of accounts for software verde staff.

--- sql/load-qa-data.sql
+++ sql/load-qa-data.sql
@@ -1,6 +1,4 @@
 INSERT INTO companies (name) VALUES ('Software Verde, LLC');
 INSERT INTO companies (name) VALUES ('Company Name, INC');
 
-INSERT INTO accounts (username, name, password, company_id) VALUES ('josh@softwareverde.com', 'Josh Green', 'aa80cf9d7cba1d315277f7d345d82b66b8a0d3dab77e91e920bd59a0f927972c', 1);
-INSERT INTO accounts (username, name, password, company_id) VALUES ('groot@softwareverde.com', 'Andrew Groot', 'aa80cf9d7cba1d315277f7d345d82b66b8a0d3dab77e91e920bd59a0f927972c', 1);
-INSERT INTO accounts (username, name, password, company_id) VALUES ('lesar@softwareverde.com', 'Eliot Lesar', 'aa80cf9d7cba1d315277f7d345d82b66b8a0d3dab77e91e920bd59a0f927972c', 1);
+INSERT INTO accounts (username, name, password, company_id) VALUES ('test@softwareverde.com', 'Pat Test', 'Argon2$2$19$12$16$2$F3EE5AB0A855880CDFCC44576D8F43F8$32$B534941D2FBBEA6EE6C460B7D10E5F8592BD39F23497C9ADAE6ED35BAF4BACA7', 1);
